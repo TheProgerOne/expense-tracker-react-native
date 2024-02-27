4d08bdb982bc754ccfa33464d4ac7c56073e3f4a
ilya <ilyagrinc@gmail.com>
2024-02-27 19:25:39 +0500

    [VOCAB-1] add Overview

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,17 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { MainDrawerNavigator } from './navigation/MainDrawerNavigator';
+import { FinancialProvider } from './FinancialContext';
 
 const App = () => {
   return (
+  <FinancialProvider> 
     <NavigationContainer>
       <MainDrawerNavigator />
       <StatusBar style="auto" />
     </NavigationContainer>
+    </FinancialProvider>
+
   );
 };
 
